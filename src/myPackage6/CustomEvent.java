958becd4e8b275cbe5b96f67b5485d29b88bdf36
dactue1006 <dactue1006@gmail.com>
2018-11-07 21:54:22 +0700

    custom event handling

---
+++ src/myPackage6/CustomEvent.java
@@ -0,0 +1,13 @@
+package myPackage6;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomEvent extends ApplicationEvent {
+	public CustomEvent(Object source) {
+		super(source);
+	}
+	
+	public String toString() {
+		return "My custom event";
+	}
+}
